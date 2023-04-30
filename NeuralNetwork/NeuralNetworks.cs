1b01117a6c119d92030a96db6df2ee00c465d919
irakli imesashvili <i.imesashvili@betsolutions.com>
2023-04-30 19:48:23 +0400

    Add project files.

---
+++ NeuralNetwork/NeuralNetworks.cs
@@ -0,0 +1,202 @@
+﻿using System;
+
+public class NeuralNetworks
+{
+    private int inputSize;
+    private int hiddenSize;
+    private int outputSize;
+
+    private double[,] inputWeights;
+    private double[] inputBiases;
+
+    private double[,] outputWeights;
+    private double[] outputBiases;
+
+    public NeuralNetworks(int inputSize, int hiddenSize, int outputSize)
+    {
+        this.inputSize = inputSize;
+        this.hiddenSize = hiddenSize;
+        this.outputSize = outputSize;
+
+        // Initialize input layer weights and biases
+        inputWeights = new double[inputSize, hiddenSize];
+        inputBiases = new double[hiddenSize];
+
+        Random rand = new Random();
+        for (int i = 0; i < inputSize; i++)
+        {
+            for (int j = 0; j < hiddenSize; j++)
+            {
+                inputWeights[i, j] = rand.NextDouble() - 0.5;
+            }
+        }
+
+        for (int j = 0; j < hiddenSize; j++)
+        {
+            inputBiases[j] = rand.NextDouble() - 0.5;
+        }
+
+        // Initialize output layer weights and biases
+        outputWeights = new double[hiddenSize, outputSize];
+        outputBiases = new double[outputSize];
+
+        for (int i = 0; i < hiddenSize; i++)
+        {
+            for (int j = 0; j < outputSize; j++)
+            {
+                outputWeights[i, j] = rand.NextDouble() - 0.5;
+            }
+        }
+
+        for (int j = 0; j < outputSize; j++)
+        {
+            outputBiases[j] = rand.NextDouble() - 0.5;
+        }
+    }
+
+    private double[] Sigmoid(double[] x)
+    {
+        double[] y = new double[x.Length];
+
+        for (int i = 0; i < x.Length; i++)
+        {
+            y[i] = 1 / (1 + Math.Exp(-x[i]));
+        }
+
+        return y;
+    }
+
+    public double[] FeedForward(double[] input)
+    {
+        double[] hidden = new double[hiddenSize];
+
+        // Calculate input layer activation
+        for (int j = 0; j < hiddenSize; j++)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < inputSize; i++)
+            {
+                sum += input[i] * inputWeights[i, j];
+            }
+
+            sum += inputBiases[j];
+            hidden[j] = sum;
+        }
+
+        // Apply sigmoid activation to hidden layer
+        hidden = Sigmoid(hidden);
+
+        double[] output = new double[outputSize];
+
+        // Calculate output layer activation
+        for (int j = 0; j < outputSize; j++)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < hiddenSize; i++)
+            {
+                sum += hidden[i] * outputWeights[i, j];
+            }
+
+            sum += outputBiases[j];
+            output[j] = sum;
+        }
+
+        // Apply sigmoid activation to output layer
+        output = Sigmoid(output);
+
+        return output;
+    }
+
+    public void Train(double[] input, double[] targetOutput, double learningRate)
+    {
+        // Calculate activations
+        double[] hidden = new double[hiddenSize];
+
+        // Calculate input layer activation
+        for (int j = 0; j < hiddenSize; j++)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < inputSize; i++)
+
+            {
+                sum += input[i] * inputWeights[i, j];
+            }
+            sum += inputBiases[j];
+            hidden[j] = sum;
+        }
+        // Apply sigmoid activation to hidden layer
+        hidden = Sigmoid(hidden);
+
+        double[] output = new double[outputSize];
+
+        // Calculate output layer activation
+        for (int j = 0; j < outputSize; j++)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < hiddenSize; i++)
+            {
+                sum += hidden[i] * outputWeights[i, j];
+            }
+
+            sum += outputBiases[j];
+            output[j] = sum;
+        }
+
+        // Apply sigmoid activation to output layer
+        output = Sigmoid(output);
+
+        // Calculate error
+        double[] outputError = new double[outputSize];
+
+        for (int j = 0; j < outputSize; j++)
+        {
+            outputError[j] = (targetOutput[j] - output[j]) * output[j] * (1 - output[j]);
+        }
+
+        double[] hiddenError = new double[hiddenSize];
+
+        for (int j = 0; j < hiddenSize; j++)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < outputSize; i++)
+            {
+                sum += outputError[i] * outputWeights[j, i];
+            }
+
+            hiddenError[j] = sum * hidden[j] * (1 - hidden[j]);
+        }
+
+        // Update output layer weights and biases
+        for (int i = 0; i < hiddenSize; i++)
+        {
+            for (int j = 0; j < outputSize; j++)
+            {
+                outputWeights[i, j] += learningRate * outputError[j] * hidden[i];
+            }
+        }
+
+        for (int j = 0; j < outputSize; j++)
+        {
+            outputBiases[j] += learningRate * outputError[j];
+        }
+
+        // Update input layer weights and biases
+        for (int i = 0; i < inputSize; i++)
+        {
+            for (int j = 0; j < hiddenSize; j++)
+            {
+                inputWeights[i, j] += learningRate * hiddenError[j] * input[i];
+            }
+        }
+
+        for (int j = 0; j < hiddenSize; j++)
+        {
+            inputBiases[j] += learningRate * hiddenError[j];
+        }
+    }
+}
---
+++ NeuralNetwork/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace NeuralNetwork
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a neural network with 2 inputs, 3 hidden neurons, and 1 output
+            NeuralNetworks net = new NeuralNetworks(2, 3, 1);
+
+            // Train the network on a set of inputs and target outputs
+            double[,] inputs = { { 1, 0 }, { 0, 1 }, { 1, 1 }, { 0, 0 } };
+            double[,] targets = { { 1 }, { 1 }, { 0 }, { 0 } };
+
+            double learningRate = 0.2;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                for (int j = 0; j < inputs.GetLength(0); j++)
+                {
+                    net.Train(new double[] { inputs[j, 0], inputs[j, 1] }, new double[] { targets[j, 0] }, learningRate);
+                }
+            }
+
+            // Test the trained network
+            double[] output = net.FeedForward(new double[] { 1, 0 });
+            Console.WriteLine(output[0]); // Expected output: close to 1
+
+            output = net.FeedForward(new double[] { 0, 1 });
+            Console.WriteLine(output[0]); // Expected output: close to 1
+
+            output = net.FeedForward(new double[] { 1, 1 });
+            Console.WriteLine(output[0]); // Expected output: close to 0
+
+            output = net.FeedForward(new double[] { 0, 0 });
+            Console.WriteLine(output[0]); // Expected output: close to 0
+
+            Console.ReadKey();
+        }
+    }
+}
